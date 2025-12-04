f7834fd82598cc06f4dcbd3a71e7437d53008afa
Kolya Tymoshchuk <kolya.tymo@gmail.com>
2025-12-04 21:46:19 +0200

    taska 1-2

--- task2.py
+++ task2.py
@@ -1,43 +1,43 @@
-# task 1
-# namb1 = int(input('number1: '))
-# namb2 = int(input('number2'))
-# namb3 = int(input('number3'))
-# print(namb1 + namb2 + namb3)
-# print(namb1 * namb2 * namb3)
+task 1
+namb1 = int(input('number1: '))
+namb2 = int(input('number2'))
+namb3 = int(input('number3'))
+print(namb1 + namb2 + namb3)
+print(namb1 * namb2 * namb3)
 
-# task 2
-# sume1 = int(input('number1 : '))
-# sume2 = int(input('number2 : '))
-# print("Retrdtrdt: ", 1 / 2 * sume1 * sume2)
+task 2
+sume1 = int(input('number1 : '))
+sume2 = int(input('number2 : '))
+print("Retrdtrdt: ", 1 / 2 * sume1 * sume2)
 
-# task 3
-# number1 = int(input('money for mounth : '))
-# number2 = int(input('bank for mounth : '))
-# number3 = int(input('komunalka : '))
-# print('ressult : ', number1 - number2 - number3)
+task 3
+number1 = int(input('money for mounth : '))
+number2 = int(input('bank for mounth : '))
+number3 = int(input('komunalka : '))
+print('ressult : ', number1 - number2 - number3)
 
-# task 4 
-# kilometer = int(input('distance : '))
-# fuel = int(input("fuel for 100km : "))
-# sume = int(input("money for fuel : "))
-# print('ressult : ', kilometer / 100 * fuel * sume)
+task 4 
+kilometer = int(input('distance : '))
+fuel = int(input("fuel for 100km : "))
+sume = int(input("money for fuel : "))
+print('ressult : ', kilometer / 100 * fuel * sume)
 
-# task 5
-# sume = int(input('sume : '))
-# people = int(input('people : '))
-# sume1 = (sume / 100 * 15)
-# sume2 = (sume + sume1)
-# print(sume + sume1, sume1)
-# print(sume2 / people)
+task 5
+sume = int(input('sume : '))
+people = int(input('people : '))
+sume1 = (sume / 100 * 15)
+sume2 = (sume + sume1)
+print(sume + sume1, sume1)
+print(sume2 / people)
 
-# task 6
-# orenda = int(input('money for a car for 1 day : '))
-# days = int(input('days : '))
-# sume = int(input('sume : '))
-# all_orenda_zastava = (orenda * days + sume)
-# all_orenda = (orenda * days)
-# orenda_for_day = (all_orenda / days)
-# print(all_orenda_zastava, all_orenda, orenda_for_day)
+task 6
+orenda = int(input('money for a car for 1 day : '))
+days = int(input('days : '))
+sume = int(input('sume : '))
+all_orenda_zastava = (orenda * days + sume)
+all_orenda = (orenda * days)
+orenda_for_day = (all_orenda / days)
+print(all_orenda_zastava, all_orenda, orenda_for_day)
 
 
 
