098280f9c2661a69753503901c54ccddaa104f23
Kolya Tymoshchuk <kolya.tymo@gmail.com>
2025-12-04 20:22:54 +0200

    taska2

--- task2.py
+++ task2.py
@@ -1,8 +1,11 @@
-namb1 = int(input('number1'))
+# task 1
+namb1 = int(input('number1: '))
 namb2 = int(input('number2'))
 namb3 = int(input('number3'))
 print(namb1 + namb2 + namb3)
+print(namb1 * namb2 * namb3)
 
+# task 2
 sume1 = input('number1')
 sume2 = input('number2')
-print(1 / 2)
+print(1 / 2 )
