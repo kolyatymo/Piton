b480c80599f4e6bc4556d94d7d8696a80d055d9b
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2025-12-04 20:03:10 +0200

    first commit

---
+++ task1.py
@@ -0,0 +1,14 @@
+print("to be")
+print("or not")
+print('to be')
+
+print("\"Life is what happens")
+print('\twhen')
+print('\t you\'re busy making others plans')
+print('\t John Lennon')
+
+print('\"The best way to predict the future')
+print('\t is to')
+print('\t \tinvent it.\"')
+print('\t \t \tAlan Kay')
+
---
+++ task2.py
@@ -0,0 +1,8 @@
+namb1 = int(input('number1'))
+namb2 = int(input('number2'))
+namb3 = int(input('number3'))
+print(namb1 + namb2 + namb3)
+
+sume1 = input('number1')
+sume2 = input('number2')
+print(1 / 2)
