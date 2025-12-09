b1376cc6e623d351a3e0f0853f41526217b662d8
Kolya Tymoshchuk <kolya.tymo@gmail.com>
2025-12-10 00:28:21 +0200

    task html and python

---
+++ klasswork3.py
@@ -0,0 +1,47 @@
+#task 1
+# people = int(input('meters : '))
+# sm = people * 100
+# ds = people * 10
+# mm = people * 1000
+# mul = people * 0.00062
+# print(sm, ds, mm, mul, sep='\n')
+
+#task 2
+# trukytnuk = int(input('weight : '))
+# trukytnuk1 = int(input('height : '))
+# p = 1 / 2 * trukytnuk * trukytnuk1
+# print(p)
+
+#task 3
+# number = int(input('number1 : '))
+# number1 = int(input('number2 : '))
+# number2 = int(input('number3 : '))
+# print('{}{}{}'.format(number, number1, number2))
+
+#task 4
+# sume = int(input('sume : '))
+# sum1 = sume // 1000
+# sum2 = (sume // 100) - sum1 * 10
+# sum3 = (sume - sum1 * 1000 - sum2 * 100)//10
+# sum4 = sume - sum1 * 1000 - sum2 * 100 - sum3 * 10
+# sum5 = sum1 * sum2 * sum3 * sum4
+# print(sum1, sum2, sum3, sum4, sum5, sep='\n')
+
+#task 5 
+# sume = int(input('sume : '))
+# sum1 = sume // 1000
+# sum2 = (sume // 100) - sum1 * 10
+# sum3 = (sume - sum1 * 1000 - sum2 * 100)//10
+# sum4 = sume - sum1 * 1000 - sum2 * 100 - sum3 * 10
+# print('{}{}{}{}'.format(sum4, sum3, sum2, sum1))
+
+#task 6
+# sume = int(input('sume : '))
+# number = int(input('sume : '))
+# sum1 = sume // 100
+# sum2 = (sume // 10) - sum1 * 10
+# sum3 = (sume - sum1 * 100 - sum2 * 10)//1
+# num = number // 100
+# num1 = (number // 10) - num * 10
+# num2 = (number - num * 100 - num1 * 10)//1
+# print('{}{}{}{}{}{}'.format(sum1, num, sum2, num1, sum3, num2))
