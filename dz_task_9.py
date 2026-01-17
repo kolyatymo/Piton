005df2e4e7f1a64bed5d228eb81eba43d0bcbc6d
Kolya Tymoshchuk <kolya.tymo@gmail.com>
2026-01-17 21:39:00 +0200

    task9

--- dz_task_9.py
+++ dz_task_9.py
@@ -13,6 +13,10 @@
 #         [k] --> малюнок к
 #         [g] --> малюнок г
 #         [v] --> малюнок в
+#         [d] --> малюнок д
+#         [e] --> малюнок е
+#         [z] --> малюнок з
+#         [j] --> малюнок ж
 #         [0] --> stop 
 #                             Enter --> ''')
 #     if pokaz == '0':
@@ -39,9 +43,10 @@
 # # img і
 
 #     if pokaz == "i":
-#         for j in range(line,0,-1):
-#             for i in range(1, j+1):
-#                 print('*', end=' ')
+#         line = 11
+#         for i in range(1, line +1):
+#             line -= 2
+#             print('*' * line, end=' ')
 #             print()
 #         print('--------- малюнок І ---------',)
 
@@ -63,18 +68,105 @@
 #         for i in range(1, line + 1):
 #             print(' ' * ((line - i)*2), end='')
 #             for j in range(1, i+1):
+    
 #                 print('*', end=' ')
 #             for j in range(i-1, 0,-1):
 #                 print('*', end=' ')
 #             print()
 #         print('--------- малюнок Г ---------',)
 
-# # img в
+# # # img в
 #     if pokaz == 'v':
+#         q = 11
+#         line = 5
 #         for i in range(1, line +1):
-#             print('*' * (line +1 - i)*2),
-#             for j in range(1, i + 1):
-#                 print(' ', end='')
+#             q -= 2
+#             print(' ' * i, end='')
+#             print('*' * q, end='')
+#             print(' ' * i, end='')
+#             print()
 #         print('--------- малюнок В ---------',)
     
 
+#     # img д
+#     if pokaz == 'd':
+#         line = 11
+#         q = 5
+#         for i in range(1, line +1):
+#             q -= 2
+#             print(' ' * i, end='')
+#             print('*' * line, end='')
+#             print(' ' * i, end='')
+#             print()
+
+#         for i in range(4, 0, -1):
+#             q += 2
+#             print(' ' * i, end='')
+#             print('*' * line, end='')
+#             print(' ' * i, end='')
+#             print()
+#         print('--------- малюнок Д ---------',)
+    
+#     # img e
+#     if pokaz == 'e':
+#         q = 10
+#         line = 5
+#         for i in range(1, line +1):
+#             q -= 2
+#             print('*' * i, end='')
+#             print(' ' * q, end='')
+#             print('*' * i, end='')
+#             print()
+
+
+#         for i in range(4, 0, -1):
+#             q += 2
+#             print('*' * i, end='')
+#             print(' ' * q, end='')
+#             print('*' * i, end='')
+#             print()
+#         print('--------- малюнок Е ---------',)
+
+
+#     # img ж
+#     if pokaz == 'j':
+#         line = 10
+#         star = 0
+#         space = line
+#         flag = True
+#         for i in range(1, line):
+#             if flag:
+#                 star += 1
+#                 space-=2
+#             else:
+#                 star -=1
+#                 space +=2
+#             if star == line // 2:
+#                 flag = False
+#             print('*' * star, end='')
+#             print(' ' * space, end='')
+#             print()
+#         print('--------- малюнок Ж ---------',)
+
+#     # img з
+#     if pokaz == 'z':
+#         line = 10
+#         star = 0
+#         space = line
+#         flag = True
+#         for i in range(1, line):
+#             if flag:
+#                 space -=1
+#                 star +=1
+#             else:
+#                 space +=1
+#                 star -=1
+#             if star == line // 2:
+#                 flag = False
+#             print(' ' * space, end='')
+#             print('*' * star, end='')
+#             print()
+#         print('--------- малюнок З ---------',)
+
+
+        
---
+++ klasswork10.py
@@ -0,0 +1,298 @@
+
+# word = 'purple'
+
+# # print(len(word))
+# # print(word[len(word)-1])
+# # print(word[0])
+
+
+# work1 = "R"
+# for i in range(1,len(word)):
+#     work1 += word[i]
+# print(work1)
+
+# print(word[-1])
+
+# [start : stop : step]
+
+# line = "HyperText Markup Language"
+
+# print(line)
+# print(line[5:15:2])
+# print(line[5::2])
+# print(line[::2])
+# print(line[:15])
+# print(line[::-1])
+# print("\n")
+
+# print(ord('A'))
+# print(chr(ord('A')+32))
+# print('\n')
+
+# print(f'Test line [{line * 2}]')
+# print('{0} test {0} method format {1}' .format('red','blue'))
+
+# print('\n')
+
+
+# print('\\n - enter; \\t - tab')
+# print(r'\n - enter; \t - tab')
+
+# url = r'c:\Users\Vlad\Downloads\home_list.zip'
+
+# print('\n')
+
+# print("Abc" == 'abc')
+# print("abd" == 'abc')
+# print("abc" == 'abc')
+# print("abd" >= 'abc')
+# print('\n')
+
+# print(line)
+# print('a' in line)
+# print('Hyper' in line)
+# print('Hyper text 'not in line)
+
+# print('\n')
+
+# number = "124353646"
+# letters = "hdgfkndf"
+# line = "Lorem ipsum emet dolor"
+# work = "BANANA"
+# letDig = "hhfhbh313fffe5"
+
+# # лише букви чи цифри
+# print("--------------- isalnum() --------------")
+
+
+# print('\t', number, '\t------>', number.isalnum())
+# print('\t', letters, '\t------>', letters.isalnum())
+# print('\t', line, '\t------>', line.isalnum())
+# print('\t', work, '\t------>', work.isalnum())
+# print('\t', letDig, '\t------>', letDig.isalnum())
+
+# print('\n')
+
+# # лише букви
+# print("--------------- isalpha() --------------")
+
+# print('\t', number, '\t------>', number.isalpha())
+# print('\t', letters, '\t------>', letters.isalpha())
+# print('\t', line, '\t------>', line.isalpha())
+# print('\t', work, '\t------>', work.isalpha())
+# print('\t', letDig, '\t------>', letDig.isalpha())
+
+
+# print('\n')
+
+# # лише цифри
+# print("--------------- isdigit() --------------")
+
+# print('\t', number, '\t------>', number.isdigit())
+# print('\t', letters, '\t------>', letters.isdigit())
+# print('\t', line, '\t------>', line.isdigit())
+# print('\t', work, '\t------>', work.isdigit())
+# print('\t', letDig, '\t------>', letDig.isdigit())
+
+
+# print('\n')
+
+# # лише пропуски
+# print("--------------- isspace() --------------")
+
+# print('\t', number, '\t------>', number.isspace())
+# print('\t', letters, '\t------>', letters.isspace())
+# print('\t', line, '\t------>', line.isspace())
+# print('\t', work, '\t------>', work.isspace())
+# print('\t', letDig, '\t------>', letDig.isspace())
+# print('\t', '    ', '\t------>', '    '.isspace())
+
+
+# print('\n')
+
+# # лише маленькі літери
+# print("--------------- islower() --------------")
+
+# print('\t', number, '\t------>', number.islower())
+# print('\t', letters, '\t------>', letters.islower())
+# print('\t', line, '\t------>', line.islower())
+# print('\t', work, '\t------>', work.islower())
+# print('\t', letDig, '\t------>', letDig.islower())
+# print('\t', '    ', '\t------>', '    '.islower())
+
+
+# print('\n')
+
+# # лише великі літери
+# print("--------------- isupper() --------------")
+
+# print('\t', number, '\t------>', number.isupper())
+# print('\t', letters, '\t------>', letters.isupper())
+# print('\t', line, '\t------>', line.isupper())
+# print('\t', work, '\t------>', work.isupper())
+# print('\t', letDig, '\t------>', letDig.isupper())
+# print('\t', '    ', '\t------>', '    '.isupper())
+
+
+# print('\n')
+
+# # титулка
+# print("--------------- istitle() --------------")
+# print('\t', number, '\t------>', number.istitle())
+# print('\t', letters, '\t------>', letters.istitle())
+# print('\t', line, '\t------>', line.istitle())
+# print('\t', work, '\t------>', work.istitle())
+# print('\t', letDig, '\t------>', letDig.istitle())
+# print('\t', '    ', '\t------>', '    '.istitle())
+# print('\t', 'Lor Em', '\t------>', 'Lor Em'.istitle())
+
+
+# print('\n')
+
+# # 
+# print("--------------- lower() --------------")
+
+# print('\t', number, '\t------>', number.lower())
+# print('\t', letters, '\t------>', letters.lower())
+# print('\t', line, '\t------>', line.lower())
+# print('\t', work, '\t------>', work.lower())
+# print('\t', letDig, '\t------>', letDig.lower())
+# print('\t', '    ', '\t------>', '    '.lower())
+# print('\t', 'Lor Em', '\t------>', 'Lor Em'.lower())
+
+
+# print('\n')
+
+# # 
+# print("--------------- upper() --------------")
+
+# print('\t', number, '\t------>', number.upper())
+# print('\t', letters, '\t------>', letters.upper())
+# print('\t', line, '\t------>', line.upper())
+# print('\t', work, '\t------>', work.upper())
+# print('\t', letDig, '\t------>', letDig.upper())
+# print('\t', '    ', '\t------>', '    '.upper())
+# print('\t', 'Lor Em', '\t------>', 'Lor Em'.upper())
+
+
+# print('\n')
+
+# # 
+# print("--------------- title() --------------")
+
+# print('\t', number, '\t------>', number.title())
+# print('\t', letters, '\t------>', letters.title())
+# print('\t', line, '\t------>', line.title())
+# print('\t', work, '\t------>', work.title())
+# print('\t', letDig, '\t------>', letDig.title())
+# print('\t', '    ', '\t------>', '    '.title())
+# print('\t', 'Lor Em', '\t------>', 'Lor Em'.title())
+
+
+# print('\n')
+
+# # 
+# print("--------------- capitalize() --------------")
+
+# print('\t', number, '\t------>', number.capitalize())
+# print('\t', letters, '\t------>', letters.capitalize())
+# print('\t', line, '\t------>', line.capitalize())
+# print('\t', work, '\t------>', work.capitalize())
+# print('\t', letDig, '\t------>', letDig.capitalize())
+# print('\t', '    ', '\t------>', '    '.capitalize())
+# print('\t', 'Lor Em', '\t------>', 'Lor Em'.capitalize())
+
+
+# print('\n')
+
+# # 
+# print("--------------- swapcase() --------------")
+
+# print('\t', number, '\t------>', number.swapcase())
+# print('\t', letters, '\t------>', letters.swapcase())
+# print('\t', line, '\t------>', line.swapcase())
+# print('\t', work, '\t------>', work.swapcase())
+# print('\t', letDig, '\t------>', letDig.swapcase())
+# print('\t', '    ', '\t------>', '    '.swapcase())
+# print('\t', 'Lor Em', '\t------>', 'Lor Em'.swapcase())
+
+
+# print('\n')
+
+# line = "ipsum Lorem ipsum emet ipsum dolor"
+# print('================= find(substr, startindex = default, endindex = default) ==============')
+# print('\t', line, '\t -----> ', line.find('ipsum'))
+# index = line.find('insum')
+# print('\t', line, '\t -----> index :: ', line.find('ipsum'), ' value :: ', line[index:index + len('ipsum')])
+# print('\t', line, '\t -----> ', line.find('ipsum', 0))
+# print('\t', line, '\t -----> ', line.find('ipsum', 1))
+# print('\t', line, '\t -----> ', line.find('ipsum', 13))
+# print('\t', line, '\t -----> ', line.find('ipsum', 24))
+
+# print('\n')
+# print('================= find(substr, startindex = default, endindex = default) ==============')
+
+# index = -1
+# while True:
+#     index = line.lower().find('ipsum', index + 1)
+#     print(index)
+#     if index == -1:
+#         break
+
+# print('\n')
+
+# line = "ipsum Lorem ipsum emet ipsum dolor"
+# print('================= rfind(substr, startindex = default, endindex = default) ==============')
+
+# print('\t', line, '\t -----> ', line.rfind('ipsum', 0))
+# print('\t', line, '\t -----> ', line.rfind('ipsum', 0, 23))
+# print('\t', line, '\t -----> ', line.rfind('ipsum', 0, 12))
+# print('\t', line, '\t -----> ', line.rfind('ipsum', 0, 0))
+
+# print('\n')
+
+# # line = "ipsum Lorem ipsum emet ipsum dolor"
+# # print('================= index(substr, startindex = default, endindex = default) ==============')
+
+# # print('\t', line, '\t -----> ', line.index('ipsum', 0))
+# # print('\t', line, '\t -----> ', line.index('ipsum', 0, 23))
+# # print('\t', line, '\t -----> ', line.index('ipsum', 0, 12))
+# # print('\t', line, '\t -----> ', line.index('ipsum', 0, 0))
+
+# # print('\n')
+
+# # line = "ipsum Lorem ipsum emet ipsum dolor"
+# # print('================= rindex(substr, startindex = default, endindex = default) ==============')
+
+# # print('\t', line, '\t -----> ', line.rindex('ipsum', 0))
+# # print('\t', line, '\t -----> ', line.rindex('ipsum', 0, 23))
+# # print('\t', line, '\t -----> ', line.rindex('ipsum', 0, 12))
+# # print('\t', line, '\t -----> ', line.rindex('ipsum', 0, 0))
+
+# print('\n')
+
+# line = "ipsum Lorem ipsum emet ipsum dolor"
+# print('================= count(substr) ==============')
+
+# print('\t', line, '\t -----> ', line.count('ipsum'))
+# print('\t', line, '\t -----> ', line.lower().count('l'))
+
+# print('\n')
+
+# line = "ipsum Lorem ipsum emet ipsum dolor"
+# print('================= replace(substr) ==============')
+
+# print('\t', line, '\t -----> ', line.replace('ipsum', 'red'))
+# line = line.replace('ipsum', 'red')
+# print(line)
+
+
+# # test = input('Enter number --> ').split()
+# # sum = 0
+
+# # for i in test:
+# #     sum += int(i)
+# # print(sum)
+
+# date = '22.03.2026'.split('.')
+# print(date[0])
